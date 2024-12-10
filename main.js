5041280de034d6f7d37f12b11649cd9b0b04e8f2
achdianaja <rizkiachdian@gmail.com>
2024-12-10 20:57:49 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+function setDapurLed() {
+    ledDapur.style.backgroundColor = "red";
+    dapurLedImage.src = "./assets/led-on.png";
+
+}
+function setTamuLed() {
+    ledTamu.style.backgroundColor = "red";
+    tamuLedImage.src = "./assets/led-on.png";
+}
+function setMakanLed() {
+    ledMakan.style.backgroundColor = "red";
+    makanLedImage.src = "./assets/led-on.png";
+}
+function setToiletLed() {
+    ledToilet.style.backgroundColor = "red";
+    toiletLedImage.src = "./assets/led-on.png";
+}
